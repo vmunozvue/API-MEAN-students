bc657c33d4fe85b4921397e2ff6667b0fa1caff9
vmunozvue <victor.munoz@col.vueling.com>
2023-04-16 14:33:38 +0200

    API

---
+++ src/routes/user.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const userSchema = require("../models/user");
+const router = express.Router();
+
+// create user
+router.post("/users", (req, res) => {
+  const user = userSchema(req.body);
+  user
+    .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// get all users
+router.get("/users", (req, res) => {
+  userSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// get a user by id
+router.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// get a user by id
+router.get("/users/email/:email", (req, res) => {
+  const { email } = req.params;
+  userSchema
+    .findOne({ email: email })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// delete a user
+router.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .deleteOne({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// delete a user
+router.delete("/users/email/:email", (req, res) => {
+  const { email } = req.params;
+  userSchema
+    .deleteOne({ email: email })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// update a user
+router.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, age, email } = req.body;
+  userSchema
+    .updateOne({ _id: id }, { $set: { name, age, email } })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+module.exports = router;
